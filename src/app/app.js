bc00024727fc7b9929ca4b04f43ba530b6abe7c9
PhilippeBA <49075036+PhilippeBA@users.noreply.github.com>
2021-04-20 12:31:56 -0400

    Ajout de la partie de Phil

--- src/app/app.js
+++ src/app/app.js
@@ -11,8 +11,13 @@ import '../assets/styles/style.scss';
 import { scroller } from './scroller';
 import stickyBits from 'stickybits'
 import { initialize as v1 } from './viz_1';
+import { initialize as v2 } from './viz_2-3';
+//import { initialize as v4 } from './viz_4';
+//import { initialize as v5 } from './viz_5';
+import { initialize as v7 } from './viz_7/viz';
 import { initialize as v8 } from './viz_8';
 import { initialize as v9 } from './viz_9';
+//import { initialize as v10 } from './viz_10';
 import { initialize as v11 } from './viz_11';
 
 // Fallback for old browsers to support sticky positioning.
@@ -23,7 +28,7 @@ let elements = [];
 stickyBits(elements, { stickyBitStickyOffset: 0 });
 
 // Initializes the scroller and the visualizations.
-Promise.all([v1(),v8(),v9(),v11()]).then(([callbacksV1,callbacksV8,callbacksV9,callbacksV11]) => {
-  scroller([callbacksV1,callbacksV8,callbacksV9,callbacksV11])
+Promise.all([v1(),v2(),v7(),v8(),v9(),v11()]).then(([callbacksV1,callbacksV2,callbacksV7,callbacksV8,callbacksV9,callbacksV11]) => {
+  scroller([callbacksV1,callbacksV2,callbacksV7,callbacksV8,callbacksV9,callbacksV11])
     .initialize();
 });
